f214896eb6feb9fa26882a5bc5a8f1b52fd884b8
h1tomo <h1.tomohero@gmail.com>
2025-04-28 11:30:57 +0900

    第2回演習課題解答例UP

---
+++ 第2回_C++の基礎/CppEx_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Let's enjoy programming!" << endl;
+}
+
+
+
+//別解
+
+/*
+#include <iostream>
+
+int main()
+{
+    std::cout << "Let's enjoy programming!" << std::endl;
+}
+*/
+
